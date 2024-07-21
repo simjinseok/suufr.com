538710c3a9b0437c15eccf7ed9f74aee5c08293f
Jinseok Sim <jinseok@simjinseok.com>
2024-07-21 18:41:45 +0900

    홈 화면에서 불필요한 섹션 제거

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/table";
-import { formatToKoreanNumber } from "@toss/utils";
-import Link from "next/link";
-import { TextLink } from "@/components/text";
 import React from "react";
 import { format } from "date-fns/format";
-import { Tab } from "@headlessui/react";
 
 const prisma = new PrismaClient();
 export default async function Page() {
@@ -90,82 +86,12 @@ export default async function Page() {
       </div>
 
       <React.Suspense>
-        <NotCheckedLessons user={user} />
-      </React.Suspense>
-
-      <React.Suspense>
         <NotPaidSyllabuses user={user} />
       </React.Suspense>
 
       <React.Suspense>
         <NotCheckedMeetings user={user} />
       </React.Suspense>
-    </div>
-  );
-}
-
-async function NotCheckedLessons({ user }: any) {
-  const notCheckedLessons = await prisma.lesson.findMany({
-    include: {
-      syllabus: {
-        include: {
-          student: true,
-        },
-      },
-    },
-    where: {
-      lessonAt: {
-        lte: new Date(),
-      },
-      isDone: false,
-      deletedAt: null,
-      syllabus: {
-        deletedAt: null,
-        student: {
-          userId: user.id,
-        },
-      },
-    },
-  });
-
-  if (notCheckedLessons.length < 1) {
-    return null;
-  }
-
-  return (
-    <div className="mt-12">
-      <Heading level={2}>완료가 필요한 레슨</Heading>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeader>학생명</TableHeader>
-            <TableHeader>일자</TableHeader>
-            <TableHeader>메모</TableHeader>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {notCheckedLessons.map((lesson) => (
-            <TableRow key={`syllabus-${lesson.id}`}>
-              <TableCell>{lesson.syllabus.student.name}</TableCell>
-              <TableCell>
-                {format(lesson.lessonAt, "yyyy-MM-dd HH:mm")}
-              </TableCell>
-              <TableCell className="whitespace-pre-wrap">
-                {lesson.notes}
-              </TableCell>
-              {/*<TableCell>*/}
-              {/*  <Link*/}
-              {/*    passHref*/}
-              {/*    legacyBehavior*/}
-              {/*    href={`/syllabuses/${lesson.id}`}*/}
-              {/*  >*/}
-              {/*    <TextLink href="">상세보기</TextLink>*/}
-              {/*  </Link>*/}
-              {/*</TableCell>*/}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
     </div>
   );
 }
