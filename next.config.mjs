d38608388e392803c240f78e2598dc663ac99ed9
Jinseok Sim <jinseok@simjinseok.com>
2026-01-12 01:02:10 +0900

    설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ export default withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
   org: 'jssim',
   project: 'suufr',
+  telemetry: false,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
