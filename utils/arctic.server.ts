94e1336e4249caeb1b33b726e828cc0397e447d0
Jinseok Sim <jinseok@simjinseok.com>
2026-01-12 03:26:53 +0900

    oauth redirect_uri 변경

--- utils/arctic.server.ts
+++ utils/arctic.server.ts
@@ -1,6 +1,6 @@
 import * as arctic from 'arctic';
 
 const domain = process.env.COGNITO_DOMAIN!;
-const cognito = new arctic.AmazonCognito(domain, process.env.COGNITO_CLIENT_ID!, process.env.COGNITO_CLIENT_SECRET!, 'http://localhost:3000/auth/cognito/callback');
+const cognito = new arctic.AmazonCognito(domain, process.env.COGNITO_CLIENT_ID!, process.env.COGNITO_CLIENT_SECRET!, process.env.COGNITO_REDIRECT_URI!);
 
 export default cognito;
