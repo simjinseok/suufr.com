892f4bcbae1d1a4751b82d0ef483f127745b56d7
Jinseok Sim <jinseok@simjinseok.com>
2026-01-23 02:21:33 +0900

    safearea 제거

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,7 +10,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  viewportFit: 'cover',
 };
 
 export default function RootLayout({
