9064f1815f16e8d9bf30eed05d5eccac03d0c81e
simjinseok <jinseok@simjinseok.com>
2026-07-06 12:55:24 +0900

    feat(api): TossPayments billing client & env setup
    빌링키 발급/자동결제 승인 fetch 래퍼(TossClient) 추가.
    웹에 @tosspayments/tosspayments-sdk 설치, 테스트 키 env 항목 추가.

--- apps/api/src/subscriptions/subscriptions.module.ts
+++ apps/api/src/subscriptions/subscriptions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
+import { TossClient } from './toss.client';
 
 @Module({
   controllers: [SubscriptionsController],
-  providers: [SubscriptionsService],
+  providers: [SubscriptionsService, TossClient],
   exports: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
